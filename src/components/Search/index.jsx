29f1291bc2e0ea28882b4bcaa3f73b54583728e4
Anton-Sleptsov <148457038+Anton-Sleptsov@users.noreply.github.com>
2024-10-24 18:53:50 +0300

    Added debounce for search

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,29 +1,44 @@
 import React from "react";
+import debounce from "lodash.debounce";
 import styles from "./Search.module.scss";
 import iconSearch from "../../assets/img/search-icon.svg";
 import iconClear from "../../assets/img/clear-icon.svg";
 import { SearchContext } from "../../layouts/Layout";
 
 export const Search = () => {
-  const { searchText, setSearchText } = React.useContext(SearchContext);
+  const { setSearchText } = React.useContext(SearchContext);
+  const [value, setValue] = React.useState("");
   const inputRef = React.useRef();
 
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const setSearchTextInContext = React.useCallback(
+    debounce((text) => {
+      setSearchText(text);
+    }, 1000),
+    []
+  );
+
+  const onChangeValue = (event) => {
+    setValue(event.target.value);
+    setSearchTextInContext(event.target.value);
+  };
+
   const onClickClear = () => {
-    setSearchText("");
+    setValue("");
     inputRef.current.focus();
-  }
+  };
 
   return (
     <div className={styles.root}>
       <img src={iconSearch} alt="" className={styles.iconSearch} />
       <input
-      ref={inputRef}
+        ref={inputRef}
         className={styles.input}
         placeholder="Поиск пицц..."
-        value={searchText}
-        onChange={(event) => setSearchText(event.target.value)}
+        value={value}
+        onChange={onChangeValue}
       />
-      {searchText && (
+      {value && (
         <img
           onClick={onClickClear}
           src={iconClear}
