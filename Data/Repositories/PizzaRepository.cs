d773073de212d74818180183a98310e5bee376b3
Anton-Sleptsov <148457038+Anton-Sleptsov@users.noreply.github.com>
2024-11-13 19:55:53 +0300

    Added endpoints for filtration of pizzas

--- Data/Repositories/PizzaRepository.cs
+++ Data/Repositories/PizzaRepository.cs
@@ -26,5 +26,61 @@ namespace Data.Repositories
         {
             return _context.Pizzas.ToList();
         }
+
+        public List<Pizza> GetAllSortAndOrder(string sortBy, string order)
+        {
+            IQueryable<Pizza> pizzas;
+            switch (sortBy)
+            {
+                case "rating":
+                    if (order == "desc")
+                    {
+                        pizzas = _context.Pizzas.OrderByDescending(pizza => pizza.Rating);
+                    }
+                    else
+                    {
+                        pizzas = _context.Pizzas.OrderBy(pizza => pizza.Rating);
+                    }
+                    break;
+                case "price":
+                    if (order == "desc")
+                    {
+                        pizzas = _context.Pizzas.OrderByDescending(pizza => pizza.Price);
+                    }
+                    else
+                    {
+                        pizzas = _context.Pizzas.OrderBy(pizza => pizza.Price);
+                    }
+                    break;
+                case "title":
+                    if (order == "desc")
+                    {
+                        pizzas = _context.Pizzas.OrderByDescending(pizza => pizza.Title);
+                    }
+                    else
+                    {
+                        pizzas = _context.Pizzas.OrderBy(pizza => pizza.Title);
+                    }
+                    break;
+                default:
+                    throw new ArgumentException("Sort does not exist");
+            }
+            return pizzas.ToList();
+        }
+
+        public List<Pizza> GetByCategory(string sortBy, string order, int category)
+        {
+            var pizzas = GetAllSortAndOrder(sortBy, order);
+            return pizzas.Where(pizza => pizza.Category == category).ToList();
+        }
+
+        public List<Pizza> GetBySearchText(string sortBy, string order, string searchText)
+        {
+            var pizzas = GetAllSortAndOrder(sortBy, order);
+            return pizzas
+                .Where(pizza => pizza.Title
+                    .Contains(searchText, StringComparison.CurrentCultureIgnoreCase))
+                .ToList();
+        }
     }
 }
--- server/Controllers/PizzaController.cs
+++ server/Controllers/PizzaController.cs
@@ -7,7 +7,7 @@ using server.Models;
 namespace server.Controllers
 {
     [ApiController]
-    [Route("{controller}/{action}")]
+    [Route("{controller}")]
     public class PizzaController : Controller
     {
         private readonly PizzaRepository _pizzaRepository;
@@ -45,6 +45,52 @@ namespace server.Controllers
         public IActionResult GetAll()
         {
             var pizzasData = _pizzaRepository.GetAll();
+
+            var pizzas = pizzasData
+                .Select(_pizzaMapper.MapViewModel);
+
+            return Ok(pizzas);
+        }
+
+        [HttpGet("GetAllSortAndOrder")]
+        public IActionResult GetAllSortAndOrder(string sortBy, string order)
+        {
+            var pizzasData = _pizzaRepository.GetAllSortAndOrder(sortBy, order);
+
+            var pizzas = pizzasData
+                .Select(_pizzaMapper.MapViewModel);
+
+            return Ok(pizzas);
+        }
+
+        [HttpGet("GetAllByCategory")]
+        public IActionResult GetAllByCategory(string sortBy, string order, int category)
+        {
+            var pizzasData = _pizzaRepository.GetByCategory(sortBy, order, category);
+
+            var pizzas = pizzasData
+                .Select(_pizzaMapper.MapViewModel);
+
+            return Ok(pizzas);
+        }
+
+        [HttpGet("GetAllBySearchText")]
+        public IActionResult GetAllBySearchText(string sortBy, string order, string searchText)
+        {
+            var pizzasData = _pizzaRepository.GetBySearchText(sortBy, order, searchText);
+
+            var pizzas = pizzasData
+                .Select(_pizzaMapper.MapViewModel);
+
+            return Ok(pizzas);
+        }
+
+        [HttpGet("GetAllByCategoryAndSearchText")]
+        public IActionResult GetAllByCategoryAndSearchText(string sortBy, string order, int category, string searchText)
+        {
+            var pizzasDataBySearch = _pizzaRepository.GetBySearchText(sortBy, order, searchText);
+            var pizzasDataByCategory = _pizzaRepository.GetByCategory(sortBy, order, category);
+            var pizzasData = pizzasDataBySearch.Intersect(pizzasDataByCategory);
 
             var pizzas = pizzasData
                 .Select(_pizzaMapper.MapViewModel);
